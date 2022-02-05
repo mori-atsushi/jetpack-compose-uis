77455cb82b3317fb13e2bb82667e2de4ee4afc9c
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-02-05 21:19:18 +0900

    Setup dependencis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,19 +22,38 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    buildFeatures {
+        compose = true
+    }
     compileOptions {
         sourceCompatibility(JavaVersion.VERSION_1_8)
         targetCompatibility(JavaVersion.VERSION_1_8)
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.0-rc03"
+    }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    sourceSets {
+        getByName("main").java.srcDir("src/main/kotlin")
+        getByName("test").java.srcDir("src/test/kotlin")
+        getByName("androidTest").java.srcDir("src/androidTest/kotlin")
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+
+    implementation("androidx.compose.ui:ui:1.1.0-rc03")
+    implementation("androidx.compose.ui:ui-tooling:1.1.0-rc03")
+    implementation("androidx.compose.foundation:foundation:1.1.0-rc03")
+    implementation("androidx.compose.material:material:1.1.0-rc03")
+
+    implementation("jp.co.cyberagent.katalog:katalog:1.1.0-beta05")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
