3170dc12071965db403bbab2e5e9d0213f78fd10
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-02-13 12:00:32 +0900

    Delete material libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 
